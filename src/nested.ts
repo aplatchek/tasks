d1770dfdbd279e307b9f6e2504293f4591c68619
aplatchek <platchek@udel.edu>
2022-02-22 20:30:04 -0500

    task 7, attempt 3

--- src/nested.ts
+++ src/nested.ts
@@ -2,7 +2,7 @@
 //import { idText } from "typescript";
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
-//import { duplicateQuestion, publishQuestion } from "./objects";
+import { duplicateQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
